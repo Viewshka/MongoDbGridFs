26100e7bc1b4f7f5a357d93f7de1957e7b40459d
Viewshka <uravip01@gmail.com>
2021-08-14 14:42:33 +0300

    Добавлено использование статичных файлов

--- FileService/Startup.cs
+++ FileService/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
@@ -53,7 +54,13 @@ namespace FileService
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "ClientApp")),
+                RequestPath = "/ClientApp"
+            });
             
             app.UseRouting();
             app.UseAuthorization();
